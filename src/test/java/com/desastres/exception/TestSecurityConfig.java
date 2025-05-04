c893023292667b28575e69bb6e270cf17dec3121
Christian Prants <chris.prants9@gmail.com>
2025-05-04 20:25:25 -0300

    Update TestSecurityConfig.java

--- src/test/java/com/desastres/exception/TestSecurityConfig.java
+++ src/test/java/com/desastres/exception/TestSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Profile("test") // Só ativa quando o perfil for 'test'
 @Configuration
