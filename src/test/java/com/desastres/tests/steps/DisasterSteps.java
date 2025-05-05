5626279a163d0e425a4fe3103c0987e53e2fc3fd
Christian Prants <chris.prants9@gmail.com>
2025-05-05 12:20:10 -0300

    Update DisasterSteps.java

--- src/test/java/com/desastres/tests/steps/DisasterSteps.java
+++ src/test/java/com/desastres/tests/steps/DisasterSteps.java
@@ -5,11 +5,15 @@ import io.cucumber.java.pt.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Map;
 
 import static org.hamcrest.Matchers.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 public class DisasterSteps {
 
     private Response response;
