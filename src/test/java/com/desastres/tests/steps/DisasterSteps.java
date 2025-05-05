cc398f07e7a1c081166e9381ef14b63fa0c48c4c
Christian Prants <chris.prants9@gmail.com>
2025-05-04 22:48:32 -0300

    Update DisasterSteps.java

--- src/test/java/com/desastres/tests/steps/DisasterSteps.java
+++ src/test/java/com/desastres/tests/steps/DisasterSteps.java
@@ -1,39 +1,55 @@
-package com.desastres.tests.steps;
+package com.desastres.tests;
 
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.Quando;
-import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.*;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import static org.hamcrest.Matchers.notNullValue;
-import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-public class DisasterSteps {
+public class ApiStepDefinitions {
 
+    private static Long userId;
+    private static Long locationId;
     private Response response;
-    private String requestBody;
 
-    @Dado("que estou autenticado como administrador")
-    public void autenticarComoAdmin() {
-        // Implementar lógica de autenticação
-    }
-
-    @Quando("envio uma requisição POST para {string} com:")
-    public void enviarRequisicaoPost(String endpoint, String body) {
-        requestBody = body;
-        response = given()
+    @Dado("que eu tenho um novo usuário com os seguintes dados:")
+    public void criarUsuario(Map<String, String> dados) {
+        String userJson = String.format("{\"nome\":\"%s\",\"email\":\"%s\",\"senha\":\"%s\",\"telefone\":%s}",
+                dados.get("nome"), dados.get("email"), dados.get("senha"), dados.get("telefone"));
+        
+        response = RestAssured.given()
             .contentType("application/json")
-            .body(body)
-            .when()
-            .post(endpoint);
+            .body(userJson)
+            .post("/api/users");
+        
+        userId = response.then().extract().path("id");
     }
 
-    @Então("a resposta deve ter status {int}")
+    @Dado("que eu tenho uma nova localização com os seguintes dados:")
+    public void criarLocalizacao(Map<String, String> dados) {
+        String locationJson = String.format("{\"nome\":\"%s\",\"latitude\":%s,\"longitude\":%s,\"tipo\":\"%s\"}",
+                dados.get("nome"), dados.get("latitude"), dados.get("longitude"), dados.get("tipo"));
+        
+        response = RestAssured.given()
+            .contentType("application/json")
+            .body(locationJson)
+            .post("/localizacoes");
+        
+        locationId = response.then().extract().path("id");
+    }
+
+    @Quando("eu envio uma requisição POST para {string} com esses dados")
+    public void enviarPost(String endpoint) {
+        // Implementação já feita nos métodos acima
+    }
+
+    @Entao("o status da resposta deve ser {int}")
     public void verificarStatus(int status) {
         response.then().statusCode(status);
     }
 
-    @Então("o corpo da resposta deve conter um ID")
-    public void verificarIdNaResposta() {
+    @Entao("a resposta deve conter um ID de usuário")
+    public void verificarIdUsuario() {
         response.then().body("id", notNullValue());
     }
+
 }
