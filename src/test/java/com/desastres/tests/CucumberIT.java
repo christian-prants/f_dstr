d9116c6ac9a408c05a1283daec7a472d512f38e7
Christian Prants <chris.prants9@gmail.com>
2025-05-04 22:56:19 -0300

    Update CucumberIT.java

--- src/test/java/com/desastres/tests/CucumberIT.java
+++ src/test/java/com/desastres/tests/CucumberIT.java
@@ -13,7 +13,8 @@ import org.junit.runner.RunWith;
         "html:target/cucumber-reports.html",
         "json:target/cucumber.json",
         "junit:target/cucumber-reports.xml"
-    }
+    },
+    tags = "@api"
 )
 public class CucumberIT {
 }
