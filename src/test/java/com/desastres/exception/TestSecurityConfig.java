6928b64884a7d6a344bf7da113aa9e7b69b5facc
Christian Prants <chris.prants9@gmail.com>
2025-05-05 12:32:00 -0300

    Update TestSecurityConfig.java

--- src/test/java/com/desastres/exception/TestSecurityConfig.java
+++ src/test/java/com/desastres/exception/TestSecurityConfig.java
@@ -1,23 +1,23 @@
 package com.desastres.exception;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
 
-@TestConfiguration
-@Profile("test")
-public class TestSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+@Profile("test") // Só ativa quando o perfil for 'test'
+@Configuration
+public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
-        return http.build();
+            .authorizeRequests().anyRequest().permitAll()
+            .and().csrf().disable();
     }
 
     @Bean
